bb440604aaa30b2ea9442eea4d98d74b9017a9cc
Lucky sharma <63166641+luckysharma05@users.noreply.github.com>
2023-02-07 18:03:40 +0530

    update url

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url = 'http://blog-app-final-api.vercel.app';
+const url = 'http://blog-app-final-api.vercel.app'; 
 
 
 let gfs, gridfsBucket;
